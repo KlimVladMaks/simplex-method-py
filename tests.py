4fa59fcfaee9a635077021350e887514768d892b
vlad <vkm.blog@yandex.ru>
2025-10-15 16:51:30 +0300

    Commit 24

--- tests.py
+++ tests.py
@@ -11,3 +11,9 @@ def test_2():
     sm.load_problem('tests_txt/test_2.txt')
     answer = sm.get_solution()
     assert answer == [[0, 0, 0, 6], 6]
+
+def test_3():
+    sm = SimplexMethod()
+    sm.load_problem('tests_txt/test_3.txt')
+    answer = sm.get_solution()
+    assert answer == [[9, 0, 0, 7], 57]
